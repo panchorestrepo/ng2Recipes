33cc61c6f33e7a48ebf064738cfba6046fc865a0
Francisco Restrepo <panchorestrepo@yahoo.com>
2016-07-17 19:59:58 -0500

    Router update to RC2

--- src/app/shared/recipe.ts
+++ src/app/shared/recipe.ts
@@ -1,12 +1,14 @@
 import {Ingredient} from "../shared/ingredient";
 
 export class Recipe {
+  id : string;
   name: string;
   content: string;
   imageUrl : string;
   ingredients : Ingredient[];
 
-  constructor(name: string, content: string, imageUrl: string, ingredients: Ingredient[]) {
+  constructor(id: string, name: string, content: string, imageUrl: string, ingredients: Ingredient[]) {
+    this.id = id;
     this.name = name;
     this.content = content;
     this.imageUrl = imageUrl;
